77517edb2213834f744c5cb351e1f6fd28049740
Bhayron Klivilan <bhayronklivilan@gmail.com>
2022-03-09 15:19:59 -0300

    Update cpf.js

--- cpf.js
+++ cpf.js
@@ -6,3 +6,13 @@ const cpfSem = cpf.replace(/[^\d]/g, "");
 //realizar a formatação...
 const cpfCom = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
 
+// formatar com cpf-cnpj-validator
+
+import { cpf } from 'cpf-cnpj-validator'
+
+
+    const CPF = "04813816193";
+
+    const novoCpf = cpf.format(CPF.cpf)
+
+    const validCpf = cpf.isValid(novoCpf)
