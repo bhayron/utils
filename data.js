bb1679134227823ad9ace4dcc32f10ad0d30498f
Bhayron Klivilan <bhayronklivilan@gmail.com>
2022-03-09 11:55:14 -0300

    Update data.js

--- data.js
+++ data.js
@@ -3,3 +3,7 @@
    let novadata = data.toLocaleDateString().replace(/[^\d]/g, '.')
 
    console.log(novadata)
+   
+   //formatacao com moment
+
+   const dataFormatada = moment(data).format('DD/MM/YYYY HH:mm:ss')
