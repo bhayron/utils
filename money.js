1a11f61df2ef83fcf28aaf6beb06b2b1fd9fb382
Bhayron Klivilan <bhayronklivilan@gmail.com>
2022-09-05 11:51:42 -0300

    Update money.js

--- money.js
+++ money.js
@@ -3,3 +3,8 @@ var numer0 = 125.45
 var dinheiro = numero.toLocaleString('pt-BR',{style:'currency', currency: 'BRL'});
 
 console.log(dinheiro);
+
+
+const str = '100,00'
+const strNum = str.replace(/[^0-9]/g, '')
+console.log(strNum) // 10000
